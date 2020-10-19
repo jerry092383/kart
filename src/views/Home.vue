ae0225a1b9f009d3524b5a1792bcd70066c3e1eb
Jerry092383 <jerry092383@gmail.com>
2020-10-19 23:46:00 +0800

    新的跑跑資訊站

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  .container-fluid
+    h2.my-3 總隊數：{{ data.length }}
+    .div(
+      v-for="(item, key) in data"
+      :key="key"
+    )
+      h3 {{ item.team_name }}
+      .row
+        div.col-sm-12.col-md-6.mb-3(
+          v-for="(player, key) in item.players"
+          :key="key"
+        )
+          .card
+            img(
+              :src="player.image_path"
+              width="100%"
+              height="300px"
+            )
+            span {{ player.game_name }} ({{ player.nick_name }}) {{ player.position_type === 2 ? '[ 候補 ]' : '' }}
+
 </template>
 
 <script lang="ts">
+import Axios from 'axios'
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld
+@Component
+export default class Home extends Vue {
+  protected data: any[] = [];
+
+  protected created () {
+    Axios.get('https://cors-anywhere.herokuapp.com/http://dc-cs-xmas-frontend-prod-f271a7c387ea79e8.elb.ap-northeast-1.amazonaws.com/api/teams')
+      .then(res => {
+        console.log('res >> ', res)
+        this.data = res.data
+      })
   }
-})
-export default class Home extends Vue {}
+}
 </script>
